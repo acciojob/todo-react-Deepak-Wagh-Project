738819cd62192778c9972e18df861db22511b573
Deepak Wagh <deeps2398wagh@gmail.com>
2023-12-03 16:34:39 +0530

    first Attempt

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [Todo,setTodo]=useState("");
+  const[TodoList,setTodoList]=useState([]);
+  function addToDoList(){
+   let list =[...TodoList,Todo];
+   setTodoList(list);
+  }
+  function deleteItemFromList(item){
+    let list =TodoList.filter((ele)=>{
+      return ele!=item;
+    })
+    setTodoList(list);
+
+  }
+  function getKey(){
+       const timeStamp=new Date().getTime();
+       const stringTimeStamp=timeStamp.toString();
+       return "item_"+stringTimeStamp+"_"+Math.random()*100;
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <div>To-Do List</div>
+       <div><input type="text" onChange={(e)=>setTodo(e.target.value)}/>
+       <button onClick={addToDoList}>Add Todo</button></div>
+       <ul className="to-do-list-container">
+       {
+        TodoList.map((ele)=>{
+          return(<li className="to-do-container" key={getKey()}> 
+            <div>{ele}</div>
+            <button  onClick={()=>deleteItemFromList(ele)}>Delete</button>
+          </li>)
+        })
+       }
+       </ul>
     </div>
   )
 }
